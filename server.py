af1358ce2b2fce6fa641f410638149eca1b2f900
iYung <ivankhyung@gmail.com>
2018-06-16 00:37:30 -0400

    added comments to train.py

--- server.py
+++ server.py
@@ -7,9 +7,16 @@ app = flask.Flask(__name__)
 #loading my model
 model = pickle.load(open("model.pkl","r"))
 
+#defining a route for only post requests
 @app.route('/', methods=['POST'])
 def index():
-    response = {}
+    #getting an array of features from the post request's body
     feature_array = request.get_json()['feature_array']
+
+    #creating a response object
+    #storing the model's prediction in the object
+    response = {}
     response['predictions'] = model.predict([feature_array]).tolist()
+
+    #returning the response object as json
     return flask.jsonify(response)
